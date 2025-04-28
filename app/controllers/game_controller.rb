8a4ded931166764040f19f0c0e8244fc0f298daf
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-04-28 18:27:45 +0000

    done

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,19 +1,49 @@
 class GameController < ApplicationController
 
   def rock
-  @random_move = ["rock", "paper", "scissors"].sample
+  @moves = ["rock", "paper", "scissors"]
+
+    @comp_move = @moves.sample
+
+      if @comp_move == "rock"
+        @outcome = "tied"
+      elsif @comp_move == "paper"
+        @outcome = "lost"
+      else @outcome = "won"
+      end
+
 
   render({ :template => "game_templates/play_rock"})
   end
 
   def paper
-    @random_move = ["rock", "paper", "scissors"].sample
+    @moves = ["rock", "paper", "scissors"]
+
+    @comp_move = @moves.sample
+
+      if @comp_move == "paper"
+        @outcome = "tied"
+      elsif @comp_move == "scissors"
+        @outcome = "lost"
+      else @outcome = "won"
+      end
+
   
     render({ :template => "game_templates/play_paper"})
     end
 
     def scissors
-      @random_move = ["rock", "paper", "scissors"].sample
+      @moves = ["rock", "paper", "scissors"]
+
+    @comp_move = @moves.sample
+
+      if @comp_move == "scissors"
+        @outcome = "tied"
+      elsif @comp_move == "rock"
+        @outcome = "lost"
+      else @outcome = "won"
+      end
+
     
       render({ :template => "game_templates/play_scissors"})
       end
