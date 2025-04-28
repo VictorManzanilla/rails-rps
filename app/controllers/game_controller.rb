9d96912e95c5e68692b23e8558e58702193931a2
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-04-28 00:51:33 +0000

    routes, controller, actions, and views set up done

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,26 @@
+class GameController < ApplicationController
+
+  def rock
+  @random_move = ["rock", "paper", "scissors"].sample
+
+  render({ :template => "game_templates/play_rock"})
+  end
+
+  def paper
+    @random_move = ["rock", "paper", "scissors"].sample
+  
+    render({ :template => "game_templates/play_paper"})
+    end
+
+    def scissors
+      @random_move = ["rock", "paper", "scissors"].sample
+    
+      render({ :template => "game_templates/play_scissors"})
+      end
+
+  def rules
+    render({ :template => "game_templates/rules"})
+
+  end
+
+end
